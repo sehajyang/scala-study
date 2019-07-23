67d7379ea932ae45b37031c1cca00d9bc609cc20
sehajyang <sehajyang@gmail.com>
2019-07-23 18:22:53 +0900

    Add : code wars build a pile of cubes 문제 풀이 추가

--- src/main/scala/code_wars/BuildAPileOfCubes.scala
+++ src/main/scala/code_wars/BuildAPileOfCubes.scala
@@ -1,4 +1,5 @@
 package code_wars
+import org.scalatest.FunSuite
 
 /**
   * @author seha
@@ -7,6 +8,22 @@ package code_wars
   */
 object BuildAPileOfCubes extends App {
 
-  def solution(m:Long):Long = {
+  def solution(m: Long): BigInt = {
+    val n=((math.sqrt(1+8* math.sqrt(m))-1)/2).toInt
+    if (math.pow(n * (n + 1) / 2, 2) != m) return -1
+    n.toInt
+  }
+
+  print(solution(24723578342962L))
+
+  class CubeTest extends FunSuite {
+
+    test("Samples") {
+      assert(BuildAPileOfCubes.solution(4183059834009L)   === 2022)
+      assert(BuildAPileOfCubes.solution(24723578342962L)  === -1)
+      assert(BuildAPileOfCubes.solution(135440716410000L) === 4824)
+      assert(BuildAPileOfCubes.solution(40539911473216L)  === 3568)
+      assert(BuildAPileOfCubes.solution(26825883955641L)  === 3218)
+    }
   }
 }
