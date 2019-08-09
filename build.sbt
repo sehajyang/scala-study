951f137b8e0af46fdead409dac296b47d319aa21
sehajyang <sehajyang@gmail.com>
2019-08-09 14:34:05 +0900

    Mod: ScalaSDK 2.13.0 -> 2.12.8

--- build.sbt
+++ build.sbt
@@ -2,6 +2,6 @@ name := "scala-study"
 
 version := "0.1"
 
-scalaVersion := "2.13.0"
+scalaVersion := "2.12.8"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8"
--- src/main/scala/code_wars/TwoToOne.scala
+++ src/main/scala/code_wars/TwoToOne.scala
@@ -1,5 +1,4 @@
 package code_wars
-import org.scalatest._
 
 /**
   * @author seha
@@ -9,27 +8,13 @@ import org.scalatest._
   */
 object TwoToOne{
 
-  def longest(s1: String, s2: String): String = {
-    (s1 + s2).toSeq.distinct.sorted.unwrap
+  def longest(s1: String, s2: String): Unit= {
+    (s1 + s2).toSeq.distinct.sorted
   }
 
   def main(args: Array[String]): Unit = {
     print(longest("loopingisfunbutdangerous", "lessdangerousthancoding"))
   }
 
-  class KataTest extends FlatSpec with Matchers {
 
-    val tests = List(
-      (("aretheyhere", "yestheyarehere"), "aehrsty"),
-      (("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu"),
-      (("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
-    )
-
-    tests.foreach {
-      case ((s1, s2), expected) =>
-        s"longest($s1, $s2)" should s"return $expected" in {
-          TwoToOne.longest(s1, s2) should be (expected)
-        }
-    }
-  }
 }
